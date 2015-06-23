918b6a641cd1e39b0f543b04f9c8b94d5a20ee1b
lekousin <jason.bienaime@sfr.fr>
2015-06-23 23:22:36 +0200

    Host fix hotfix
    Forgot to readd armor spoofer

--- HoxHUD Hook/lib/Lua/ArmorOverhaul/lib/managers/blackmarketmanager.lua
+++ HoxHUD Hook/lib/Lua/ArmorOverhaul/lib/managers/blackmarketmanager.lua
@@ -1,5 +1,20 @@
 local outfit_string_orig = BlackMarketManager.outfit_string
 
+Global.real_armor = {
+    "level_8",
+    "level_9",
+    "level_10",
+    "level_11",
+    "level_12"
+}
+Global.spoof_armor = {
+    "level_6",
+    "level_6",
+    "level_1",
+    "level_7",
+    "level_1"
+}
+
 function BlackMarketManager:outfit_string()
 	local s = outfit_string_orig(self)
 	local count
