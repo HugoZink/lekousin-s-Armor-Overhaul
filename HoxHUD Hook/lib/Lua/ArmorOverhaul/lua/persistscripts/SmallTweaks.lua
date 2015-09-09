42dafdccdc24013c44e3a3c841fc812f172319d9
lekousin <jason.bienaime@sfr.fr>
2015-09-09 10:56:59 +0200

    Sniper damage buff removal
    2x sniper damage was completely Overkill, as it can oneshot EOD in OK
    and below

--- HoxHUD Hook/lib/Lua/ArmorOverhaul/lua/persistscripts/SmallTweaks.lua
+++ HoxHUD Hook/lib/Lua/ArmorOverhaul/lua/persistscripts/SmallTweaks.lua
@@ -2,10 +2,6 @@ if not tweak_data then return end
 if not tweak_data.character then return end
 if SmallTweaks then return end
 
-for i, range in ipairs(tweak_data.character.presets.weapon.sniper.m4.FALLOFF) do
-	range.dmg_mul = range.dmg_mul * 2
-end
-
 tweak_data.upgrades.specialization_descs[Global.spec_length] = {
 	{
 		multiperk = "1%",
