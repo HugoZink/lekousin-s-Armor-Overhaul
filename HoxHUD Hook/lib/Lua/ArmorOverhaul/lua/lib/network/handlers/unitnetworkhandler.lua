ad3629630b1a8fb5f9d85b3f54e1bc66ee7384db
Hugo Zink <hugo.zink@hotmail.com>
2020-08-03 23:45:41 +0200

    Removed HoxHud hook

--- HoxHUD Hook/lib/Lua/ArmorOverhaul/lua/lib/network/handlers/unitnetworkhandler.lua
+++
@@ -1,9 +0,0 @@
-function UnitNetworkHandler:add_synced_crew_bonus(category, upgrade, level, sender)
-	local sender_peer = self._verify_sender(sender)
-	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not sender_peer then
-		return
-	end
-
-	local peer_id = sender_peer:id()
-	managers.player:add_synced_crew_bonus(peer_id, category, upgrade, level)
-end
